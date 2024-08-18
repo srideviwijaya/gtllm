600955c7ad537185d57114b6c921edff3c2876b5
srideviwijaya <sri.deviwijaya@gmail.com>
2024-08-18 15:29:48 +0800

    updated config files

--- chatui/app.py
+++ chatui/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import Flask-CORS
 import requests
-# import os
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
